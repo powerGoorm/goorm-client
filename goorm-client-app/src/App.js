72157b04a10209e9ecf9f8dbd66934237db4e089
gimye <gimyedev@gmail.com>
2024-01-30 18:12:15 +0900

    react-router-dom 설치 및 라우팅

--- goorm-client-app/src/App.js
+++ goorm-client-app/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from "./routes/Home";
+import Auth from "./routes/Auth";
+import Board from "./routes/Board";
+import Members from "./routes/Members";
+import PBL from "./routes/PBL";
 
 function App() {
   return (
-    <div className="App">
-      start App
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Auth />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Board" element={<Board />} />
+        <Route path="/Members" element={<Members />} />
+        <Route path="/PBL" element={<PBL />} />
+      </Routes>
+    </Router>
   );
 }
 
