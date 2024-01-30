3c4f680d0a4ec878b1205b4992936b9ae8ae8ab5
gimye <gimyedev@gmail.com>
2024-01-30 18:29:39 +0900

    routing and delete useless code

--- goorm-client-app/src/routes/Board.js
+++ goorm-client-app/src/routes/Board.js
@@ -1,3 +1,12 @@
-const Board = () => <span>Board</span>;
+import React from "react";
+
+const Board = () => {
+  return (
+    <div>
+      <h1> Board </h1>
+      <p> 게시판 화면입니다. </p>
+    </div>
+  )
+}
 
 export default Board;
--- goorm-client-app/src/routes/Members.js
+++ goorm-client-app/src/routes/Members.js
@@ -1,3 +1,12 @@
-const Members = () => <span>Members</span>;
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1> Members </h1>
+      <p> 멤버 소개 화면입니다. </p>
+    </div>
+  )
+}
 
 export default Members;
