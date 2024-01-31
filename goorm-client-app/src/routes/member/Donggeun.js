0ec516d7d7abdff1ba9f250c33b9579e3622ef10
gimye <gimyedev@gmail.com>
2024-01-31 16:07:12 +0900

    Home export 오류 해결 및 멤버별 개인 페이지 라우팅 완료

---
+++ goorm-client-app/src/routes/member/Donggeun.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Donggeun = () => {
+  return (
+    <div>
+      <h1> 동근 </h1>
+      <p> 동근 님의 개인 페이지입니다. </p>
+    </div>
+  )
+}
+
+export default Donggeun;
---
+++ goorm-client-app/src/routes/member/Mingyu.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Mingyu = () => {
+  return (
+    <div>
+      <h1> 민규 </h1>
+      <p> 민규 님의 개인 페이지입니다. </p>
+    </div>
+  )
+}
+
+export default Mingyu;
