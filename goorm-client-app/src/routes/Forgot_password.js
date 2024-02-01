a87c911f337217a0cc04aa23c22fde0569818855
조우석 <dntjrwh@gmail.com>
2024-02-01 15:25:12 +0900

    회원가입, 비밀번호찾기 js 파일추가

---
+++ goorm-client-app/src/routes/Forgot_password.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Forgot_password = () => {
+  return (
+    <div>
+      <h1> Forgot_password </h1>
+      <p> Forgot_password 소개 화면입니다. </p>
+    </div>
+  )
+}
+
+export default Forgot_password;
