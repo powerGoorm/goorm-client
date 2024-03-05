8af34d0ba6b4bac8c11473d129cf0e633e7aab5d
조우석 <dntjrwh@gmail.com>
2024-03-05 15:03:52 +0900

    react-bootstrap 적용

--- goorm-client-app/src/App.js
+++ goorm-client-app/src/App.js
@@ -1,6 +1,7 @@
 // App.js
 import React, { useState } from "react";
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./components/header/Header";
